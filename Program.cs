5ee1ed45d16cd2fd20819dca22d80b7622047c73
grigoriyMirakyan <grigoriy66660@gmail.com>
2022-06-29 23:59:44 +0300

    черновой, но рабочий вариант решения

--- Program.cs
+++ Program.cs
@@ -21,38 +21,55 @@
 
 
 
-string[] FillArray()
+//string[] FillArray()
+//{
+string[] arrayStr;
+int count = 0;
+int count2 = 0;
+string str = string.Empty;
+string[] arrayStr2;
+string[] arrayStr3;
+string[] arrayStr4;
+Console.WriteLine("Введите строки через Enter. Для завершения ввода нажмите Enter дважды.");
+
+arrayStr = new string[count];
+arrayStr3 = new string[count];
+do
 {
-    string[] arrayStr;
-    int count = 0;
-    string str = string.Empty;
-    string[] arrayStr2;
-
-    Console.WriteLine("Введите строки через Enter. Для завершения ввода нажмите Enter дважды.");
-
-    arrayStr = new string[count];
-
-    do
+    str = Console.ReadLine();
+    if (str != "")
     {
-        str = Console.ReadLine();
-        if (str != "")
-        {
-            count++;
-            arrayStr2 = new string[count];
+        count++;
+        arrayStr2 = new string[count];
 
-            for (int i = 0; i < arrayStr2.Length - 1; i++)
-                arrayStr2[i] = arrayStr[i];
-            arrayStr2[count - 1] = str;
-            arrayStr = arrayStr2;
-        }
+        for (int i = 0; i < arrayStr2.Length - 1; i++)
+            arrayStr2[i] = arrayStr[i];
+        arrayStr2[count - 1] = str;
+        arrayStr = arrayStr2;
     }
-    while
-    (str != "");
-    return arrayStr;
+    if (str != "" && str.Length <= 3)
+    {
+        count2++;
+        arrayStr4 = new string[count2];
+
+        for (int i = 0; i < arrayStr4.Length - 1; i++)
+            arrayStr4[i] = arrayStr3[i];
+        arrayStr4[count2 - 1] = str;
+        arrayStr3 = arrayStr4;
+    }
+
 }
+while
+(str != "");
+//return arrayStr;
+//}
 
-string[] arrayString = FillArray();
-
-for (int i = 0; i < arrayString.Length; i++)
-    Console.Write($"{arrayString[i] + " "}");
+//string[] arrayString = FillArray();
+Console.Write("[ ");
+for (int i = 0; i < arrayStr.Length; i++)
+    Console.Write($"{arrayStr[i] + ", "}");
+Console.Write("] -> [ ");
+for (int i = 0; i < arrayStr3.Length; i++)
+    Console.Write($"{arrayStr3[i] + ", "}");
+Console.Write("]");
 Console.ReadKey();
